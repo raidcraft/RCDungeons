d44df79b7a28c12fc7a23bc3b52faaf8bd308224
Silthus <silthus@raid-craft.de>
2013-06-15 18:50:11 +0200

    initial commit

---
+++ src/main/java/de/raidcraft/dungeons/DungeonsPlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.dungeons;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Silthus
+ */
+public class DungeonsPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}

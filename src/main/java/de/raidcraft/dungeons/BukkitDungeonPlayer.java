1eb7a1364260261cd1c97ff46f649a2bbf61c728
Silthus <silthus@raid-craft.de>
2013-10-09 16:36:23 +0200

    implemented dungeon player

--- src/main/java/de/raidcraft/dungeons/BukkitDungeonPlayer.java
+++ src/main/java/de/raidcraft/dungeons/BukkitDungeonPlayer.java
@@ -1,46 +1,91 @@
 package de.raidcraft.dungeons;
 
+import com.avaje.ebean.EbeanServer;
+import de.raidcraft.RaidCraft;
 import de.raidcraft.dungeons.api.AbstractDungeonPlayer;
 import de.raidcraft.dungeons.api.Dungeon;
 import de.raidcraft.dungeons.api.DungeonInstance;
 import de.raidcraft.dungeons.api.DungeonReason;
+import de.raidcraft.dungeons.tables.TDungeonInstancePlayer;
 import de.raidcraft.dungeons.tables.TDungeonPlayer;
+import de.raidcraft.dungeons.types.PersistantDungeonInstance;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Silthus
  */
 public class BukkitDungeonPlayer extends AbstractDungeonPlayer {
 
+    private final Map<Dungeon, DungeonInstance> instances = new HashMap<>();
+    private DungeonInstance activeInstance;
+
     public BukkitDungeonPlayer(TDungeonPlayer player) {
 
         super(player.getId(), player.getName());
         setLastPosition(player.getLastPosition());
+        DungeonManager dungeonManager = RaidCraft.getComponent(DungeonManager.class);
+        for (TDungeonInstancePlayer instance : player.getInstances()) {
+            PersistantDungeonInstance dungeonInstance =
+                    new PersistantDungeonInstance(instance.getInstance(), dungeonManager.getDungeon(instance.getInstance().getDungeon().getName()));
+            instances.put(dungeonInstance.getDungeon(), dungeonInstance);
+            if (dungeonInstance.isActive()) {
+                activeInstance = dungeonInstance;
+            }
+        }
     }
 
     @Override
     public List<DungeonInstance> getDungeonInstances() {
-        //TODO: implement
+
+        return new ArrayList<>(this.instances.values());
     }
 
     @Override
     public DungeonInstance getDungeonInstance(Dungeon dungeon) {
-        //TODO: implement
+
+        return this.instances.get(dungeon);
     }
 
     @Override
     public DungeonInstance getActiveInstance() {
-        //TODO: implement
+
+        return activeInstance;
     }
 
     @Override
     public void leaveActiveDungeon(DungeonReason reason) {
-        //TODO: implement
+
+        if (getActiveInstance() != null) {
+            Player player = Bukkit.getPlayer(getName());
+            if (player != null) {
+                player.teleport(getLastPosition());
+            }
+            getActiveInstance().removePlayer(this);
+            activeInstance = null;
+        }
     }
 
     @Override
     public void save() {
-        //TODO: implement
+
+        EbeanServer database = RaidCraft.getDatabase(DungeonsPlugin.class);
+        TDungeonPlayer player = database.find(TDungeonPlayer.class, getId());
+        Location position = getLastPosition();
+        player.setLastWorld(position.getWorld().getName());
+        player.setLastX(position.getX());
+        player.setLastY(position.getY());
+        player.setLastZ(position.getZ());
+        player.setLastYaw((long) position.getYaw());
+        player.setLastPitch((long) position.getPitch());
+        for (DungeonInstance instance : getDungeonInstances()) {
+            instance.save();
+        }
     }
 }

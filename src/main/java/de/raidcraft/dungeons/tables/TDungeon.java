354e4570fc1c371f6d407d80629185424f386faa
Michael Reichenbach <michael@mirenoba.com>
2018-09-21 11:45:18 +0200

    fix(ebean): remove old ebean references

--- src/main/java/de/raidcraft/dungeons/tables/TDungeon.java
+++ src/main/java/de/raidcraft/dungeons/tables/TDungeon.java
@@ -1,15 +1,10 @@
 package de.raidcraft.dungeons.tables;
 
-import com.avaje.ebean.validation.NotNull;
+import io.ebean.annotation.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
